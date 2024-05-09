52a61c13d655b0cc9fcac31fa9130d8553d45ef5
gianganh1356 <pudleduck1pun0suy11@hotmail.com>
2024-05-09 08:22:29 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const intersection = (a, b) => {
-  const s = new Set(b);
-  return a.filter((x) => s.has(x));
-};
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
+  }
+  return prefix;
+}
